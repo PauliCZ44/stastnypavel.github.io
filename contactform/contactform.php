68b9cf445cd8ac157a934e31efbf243bd350fed2
PauliCZ44 <62062095+PauliCZ44@users.noreply.github.com>
2020-04-03 16:55:15 +0200

    Add files via upload
    New contact

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -6,24 +6,36 @@
   * For more info and help: https://templatemag.com/php-mail-form/
   */
 
-  if( file_exists($php_mail_form_library = '../lib/php-mail-form/php-mail-form.php' )) {
-    include( $php_mail_form_library );
-  } else {
-    die( 'Unable to load the PHP Mail Form Library!');
+  if(isset($_POST['submit']})){
+      $name = $_POST['name'];
+      $subject = $_POST['subject'];
+      $mailFrom = $_POST['mail'];
+      $message = $_POST['message'];
+
+      $mailTo = "stastnyp@seznam.cz";
+      $headers = "From: ".$mailFrom;
+      $txt = "You have received an e-mail from ".$name.".\n\n".$message;
+
+      mail($mailTo, $subject, $txt, $headers);
+      header("Loactin: index.php?mailsend");
   }
 
-  $contactform = new PHP_Mail_Form;
-  $contactform->ajax = true;
+  /*
+    $contactform = new PHP_Mail_Form;
+    $contactform->ajax = true;
 
 
-  $contactform->to = 'stastnyp@seznam.cz';
-  $contactform->from_name = $_POST['name'];
-  $contactform->from_email = $_POST['email'];
-  $contactform->subject = $_POST['subject'];
+    $contactform->to = 'stastnyp@seznam.cz';
+    $contactform->from_name = $_POST['name'];
+    $contactform->from_email = $_POST['email'];
+    $contactform->subject = $_POST['subject'];
 
-  $contactform->add_message( $_POST['name'], 'From');
-  $contactform->add_message( $_POST['email'], 'Email');
-  $contactform->add_message( $_POST['message'], 'Message', 10);
+    $contactform->add_message( $_POST['name'], 'From');
+    $contactform->add_message( $_POST['email'], 'Email');
+    $contactform->add_message( $_POST['message'], 'Message', 10);
 
-  echo $contactform->send();
+    echo $contactform->send();
+  ?>
+  */
+
 ?>
